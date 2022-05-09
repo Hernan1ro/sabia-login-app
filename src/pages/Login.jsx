2867df6e64c546c443054058f76997eeb4dd3237
Hernán D. Mercado <hernandmf@gmail.com>
2022-05-09 09:17:09 -0500

    styling the form component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../styles/Login.css";
 import sabiaLogo from "../assets/logos/logo_fucsia.png";
 import sabiaBanner from "../assets/images/sabiaBanner.png";
+import formLogo from "../assets/illustrations/undraw_personal_finance_tqcd.svg";
 
 const Login = () => {
   return (
@@ -12,7 +13,35 @@ const Login = () => {
           <img src={sabiaBanner} alt="sabia-family-image" />
         </div>
         <div className="main-right">
-          <div className="form-container"></div>
+          <div className="form-container">
+            <img src={formLogo} alt="form-logo" />
+            <h3>Inicio de sesión</h3>
+            <p>
+              ¿No tienes una cuenta? <strong>Registrate</strong>
+            </p>
+            <form action="">
+              <div className="email">
+                <label htmlFor="email">Correo Electrónico</label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Tu email"
+                  required
+                />
+              </div>
+              <div className="password">
+                <label htmlFor="password">Contraseña</label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Tu contraseña"
+                  required
+                />
+              </div>
+              <button type="submit">Inicia Sesión</button>
+            </form>
+            <p>¿Olvidaste tu contraseña?</p>
+          </div>
         </div>
       </div>
     </main>
