e9a2112bc1b912c4c2dfe8ad96c22ab76887bde8
Hernán D. Mercado <hernandmf@gmail.com>
2022-05-09 07:44:14 -0500

    adding the first half of the login layout

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "../styles/Login.css";
+import sabiaLogo from "../assets/logos/logo_fucsia.png";
+import sabiaBanner from "../assets/images/sabiaBanner.png";
 
 const Login = () => {
   return (
-    <main>
-      <h1>Login</h1>
+    <main className="main">
+      <div className="main__container container">
+        <div className="main-left">
+          <img src={sabiaLogo} alt="sabia-logo" />
+          <img src={sabiaBanner} alt="sabia-family-image" />
+        </div>
+        <div className="main-right">
+          <div className="form-container"></div>
+        </div>
+      </div>
     </main>
   );
 };
