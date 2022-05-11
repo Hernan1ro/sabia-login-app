f57adae753c28ce973206b09730c97165b841312
Hernán D. Mercado <hernandmf@gmail.com>
2022-05-11 12:30:08 -0500

    styling 404page

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,5 +1,16 @@
+import "../styles/Page404.css";
+import sabiaLogo from "../assets/logos/logo_fucsia.png";
+
 const Page404 = () => {
-  return <h1>Page404</h1>;
+  return (
+    <div className="page404">
+      <div>
+        <h1>Error 404</h1>
+        <img src={sabiaLogo} alt="Sabia logo" />
+        <h2>Página no encontrada</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Page404;
