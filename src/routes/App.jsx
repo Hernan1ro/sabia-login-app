5a10847939d265b419d5e30cd396d53fe0243db0
Hernán D. Mercado <hernandmf@gmail.com>
2022-05-11 01:23:00 -0500

    adding protected routes to the app

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,13 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Page404 from "../pages/Page404";
 import Main from "../pages/Main";
+import useAccessToken from "../hooks/useAccessToken";
 
 const App = () => {
+  const { token } = useAccessToken();
+  console.log(token);
   return (
     <BrowserRouter>
       <Routes>
+        {!token && <Route path="/main" element={<Navigate to="/" />} />}
+        {token && <Route path="/" element={<Navigate to="/main" />} />}
+        {token && <Route path="/register" element={<Navigate to="/main" />} />}
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/main" element={<Main />} />
